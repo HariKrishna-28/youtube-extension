3c0e119dbe3dd33d5b408d62a44d78ea546834c3
Harikrishna-28 <harikrishnac2002@gmail.com>
2024-04-10 00:56:25 +0530

    add logging

--- server/server.py
+++ server/server.py
@@ -4,8 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from youtube_transcript_api import YouTubeTranscriptApi
 from redis import Redis
 import json
+import logging
 
 app = FastAPI()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,17 +21,19 @@ app.add_middleware(
 )
 
 
-redis_client = Redis(host='localhost', port=6379)
+redis_client = Redis(host="localhost", port=6379)
 
 
-# @app.on_event("startup")
-# async def startup_event():
-#     app.state.redis = redis_client
-#     print("Server up")
+@app.on_event("startup")
+async def startup_event():
+    # app.state.redis = redis_client
+    logger.info("starting api")
+
 
 # @app.on_event("shutdown")
 # async def shutdown_event():
 #     app.state.redis.close()
+
 
 def get_redis():
     return redis_client
@@ -35,15 +43,13 @@ def get_redis():
 async def root(video_id: str, redis: Redis = Depends(get_redis)):
     try:
         # value = app.state.redis.get('transcriptData')
-        value = redis.get('transcriptData')
+        value = redis.get("transcriptData")
         if value is None:
-            transcript_data = YouTubeTranscriptApi.get_transcript(
-                video_id=video_id)
+            transcript_data = YouTubeTranscriptApi.get_transcript(video_id=video_id)
             # app.state.redis.set('transcriptData', json.dumps(transcript_data))
-            redis.set('transcriptData', json.dumps(transcript_data))
+            redis.set("transcriptData", json.dumps(transcript_data))
 
         return JSONResponse(status_code=200, content=json.loads(value))
-
 
     except Exception as e:
         raise HTTPException(status_code=500, detail={"message": str(e)})
